b5b517c0522338cc0ad71d1eadc03bd34d76b803
Jeffrey Ngundo <nyiwaj@gmail.com>
2023-01-02 03:13:25 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <studio.h>
+
 /**
  * main - prints a given phrase
  * return: 0 if exited properly, non-zero otherwise
  */
+
 int main(void)
 { 
 	puts("\"Programming is like building a multilingual puzzle");
