e5ba8e2eaa854ea3d890c497fd9d2c6420dc1bd4
Jeffrey Ngundo <nyiwaj@gmail.com>
2023-01-02 17:20:20 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all single digit numbers
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++
+			printf("%d", num);
+	putchar('\n');
+	return (0);
+}
