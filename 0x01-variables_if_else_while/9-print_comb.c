2cfd30f7dba29b80ff665025bca6ea50e8c59c07
Jeffrey Ngundo <nyiwaj@gmail.com>
2023-01-02 18:21:07 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all combinations of single digit numbers
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
