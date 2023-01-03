6da239de0ce8114de590712501767ca4ba90afb7
Jeffrey Ngundo <nyiwaj@gmail.com>
2023-01-02 18:38:40 -0800

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - A program that prints different combinations of two digits
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int ten;
+	int one;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (one = ten + 1; one <= 9; one++)
+		{
+			putchar(ten + '0');
+			putchar(one + '0');
+			if (tens < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
